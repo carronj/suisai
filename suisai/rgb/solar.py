5639d1904a67cd95b7f8ab3ab977440c58b991a4
Yuki Omori <ac.yomori@xoverlogin1.lcrc.anl.gov>
2022-05-21 18:14:18 -0500

    added solar

---
+++ suisai/rgb/solar.py
@@ -0,0 +1,10 @@
+import matplotlib
+
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ['#43676b','#80aba9','w',"#d8a373",'#9f563a'])
+rgbtemp = cmap(np.linspace(0, 1, 512))[np.newaxis, :, :3][0]
+np.savetxt('forestdawn-rgb.txt', rgbtemp)
+
+cmap = matplotlib.colors.LinearSegmentedColormap.from_list("", ['#9f563a',"#d8a373",'w'])
+rgbtemp = cmap(np.linspace(0, 1, 512))[np.newaxis, :, :3][0]
+np.savetxt('solar-rgb.txt', rgbtemp)
+
